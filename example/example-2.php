00a14d59e027d36956b5e7c7ac5054f78f3f1f61
khalyomede <>
2018-11-09 23:08:34 +0100

    added required validation example

---
+++ example/example-2.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Example 2: testing if an element is present
+ */
+require __DIR__ . '/../vendor/autoload.php';
+
+use Khalyomede\Validator;
+use Khalyomede\Exception\UnknownRuleException;
+
+$validator = new Validator(['name' => ['required', 'string']]);
+
+try {
+    $validator->validate(['name' => 'John']);
+
+    var_dump($validator->failed()); // false
+}
+catch( UnknownRuleException $exception ) {
+    echo "rule {$exception->getRule()} does not exist";
+
+    exit(1);
+}
+?>
