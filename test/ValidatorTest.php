3d61f2e60fcab83cd49f39debe55e168f5194605
khalyomede <>
2018-11-11 17:24:58 +0100

    fixed email test & added extends method tests

--- test/ValidatorTest.php
+++ test/ValidatorTest.php
@@ -1,6 +1,7 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use Khalyomede\Validator;
+use Khalyomede\Exception\RuleAlreadyExistException;
 
 class ValidatorTest extends TestCase {
     // string
@@ -138,11 +139,51 @@ class ValidatorTest extends TestCase {
     }
 
     public function testFailingEmail2() {
-        $validator = new Validator(['emaile' => ['email']]);
+        $validator = new Validator(['email' => ['email']]);
 
         $validator->validate(['email' => 'khalyomede@gmail']);
 
         $this->assertEquals($validator->failed(), true);
+    }
+
+    public function testExtends() {        
+        Validator::extends('url', function($value, $key, $items) {
+            return filter_var($value, FILTER_VALIDATE_URL) !== false;
+        });
+
+        $validator = new Validator(['twitter' => ['url']]);
+
+        $validator->validate(['twitter' => 'https://twitter.com/laravelphp']);
+
+        $this->assertEquals($validator->failed(), false);
+    }
+
+    public function testFailingExtends() {
+        Validator::extends('url2', function($value, $key, $items) {
+            return filter_var($value, FILTER_VALIDATE_URL) !== false;
+        });
+
+        $validator = new Validator(['twitter' => ['url2']]);
+
+        $validator->validate(['twitter' => 'Laravel']);
+
+        $this->assertEquals($validator->failed(), true);
+    }
+
+    public function testFailingExtends2() {
+        $this->expectException(RuleAlreadyExistException::class);
+
+        Validator::extends('string', function($value, $key, $items) {
+            return isset($items[$key]) === true && is_string($value) === true;
+        });
+    }
+
+    public function testFailingExtends3() {
+        $this->expectExceptionMessage('rule string already exist');
+
+        Validator::extends('string', function($value, $key, $items) {
+            return isset($items[$key]) === true && is_string($value) === true;
+        });
     }
 }
 ?>
