639a08571d0680da0efba6729f3d850a0bf3b18a
khalyomede <>
2018-11-11 12:34:10 +0100

    added email rule tests

--- test/ValidatorTest.php
+++ test/ValidatorTest.php
@@ -119,5 +119,30 @@ class ValidatorTest extends TestCase {
 
         $this->assertEquals($validator->failed(), true);
     }
+
+    // email
+    public function testEmail() {
+        $validator = new Validator(['email' => ['email']]);
+
+        $validator->validate(['email' => 'khalyomede@gmail.com']);
+
+        $this->assertEquals($validator->failed(), false);
+    }
+
+    public function testFailingEmail() {
+        $validator = new Validator(['email' => ['email']]);
+
+        $validator->validate(['email' => 42]);
+
+        $this->assertEquals($validator->failed(), true);
+    }
+
+    public function testFailingEmail2() {
+        $validator = new Validator(['emaile' => ['email']]);
+
+        $validator->validate(['email' => 'khalyomede@gmail']);
+
+        $this->assertEquals($validator->failed(), true);
+    }
 }
 ?>
