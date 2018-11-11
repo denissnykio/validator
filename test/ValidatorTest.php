2352b91e6a54ed648d69dbe7dec592af06982543
khalyomede <>
2018-11-11 11:29:17 +0100

    added failing tests for required rule

--- test/ValidatorTest.php
+++ test/ValidatorTest.php
@@ -27,6 +27,22 @@ class ValidatorTest extends TestCase {
         $this->assertEquals($validator->failed(), false);
     }
 
+    public function testfailingRequired() {
+        $validator = new Validator(['name' => ['required']]);
+
+        $validator->validate(['name' => null]);
+
+        $this->assertEquals($validator->failed(), true);
+    }
+
+    public function testFailingRequired2() {
+        $validator = new Validator(['name' => ['required']]);
+
+        $validator->validate(['foo' => 'John']);
+
+        $this->assertEquals($validator->failed(), true);
+    }
+
     public function testFilled() {
         $validator = new Validator(['name' => ['filled']]);
 
