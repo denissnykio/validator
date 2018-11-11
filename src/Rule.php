182607216c4e439a4d79eecd977b749705157999
khalyomede <>
2018-11-11 12:33:51 +0100

    added proper rule class

---
+++ src/Rule.php
@@ -0,0 +1,37 @@
+<?php
+namespace Khalyomede;
+
+use ReflectionClass;
+
+/**
+ * Modelize the rules for validating an array.
+ * 
+ * @category Helper
+ * 
+ * @package Validator
+ * 
+ * @author Khalyomede <khalyomede@gmail.com>
+ * 
+ * @license https://opensource.org/licenses/MIT MIT
+ * 
+ * @link https://github.com/khalyomede/validator/blob/master/src/Rule.php
+ */
+class Rule {
+    const STRING = 'string';
+    const ARRAY = 'array';
+    const REQUIRED = 'required';
+    const FILLED = 'filled';
+    const UPPER = 'upper';
+    const EMAIL = 'email';
+
+    /**
+     * Returns all the rules.
+     * 
+     * @return array<array>
+     */
+    public static function all(): array 
+    {
+        return array_values((new ReflectionClass(self::class))->getConstants());
+    }
+}
+?>
