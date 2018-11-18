a3b92a1c6214c77e77b806c221236fbadf7d6855
khalyomede <>
2018-11-18 19:09:14 +0100

    added unit tests over list of filled values

--- test/Validator/FilledTest.php
+++ test/Validator/FilledTest.php
@@ -3,12 +3,44 @@ use PHPUnit\Framework\TestCase;
 use Khalyomede\Validator;
 
 class FilledTest extends TestCase {
+    /**
+     * @var Khalyomede\Validator
+     */
+    protected $validator;
+
+    /**
+     * @var Khalyomede\Validator
+     */
+    protected $validator2;
+
+    public function setUp()
+    {
+        $this->validator = new Validator([
+            'name' => ['filled']
+        ]);
+
+        $this->validator2 = new Validator([
+            'people.*.name' => ['filled']
+        ]);
+    }
+
     public function testFilled() {
-        $validator = new Validator(['name' => ['filled']]);
+        $this->validator->validate(['name' => 'John']);
 
-        $validator->validate(['name' => 'John']);
+        $this->assertEquals($this->validator->failed(), false);
+    }
 
-        $this->assertEquals($validator->failed(), false);
+    public function testFilledList()
+    {
+        $this->validator2->validate([
+            'people' => [
+                ['name' => 'John', 'age' => 42],
+                ['name' => 'Foo', 'age' => 38],
+                ['name' => 'Bar', 'age' => 25]
+            ]
+        ]);
+
+        $this->assertFalse($this->validator2->failed());
     }
 
     public function testFailingFilled() {
@@ -25,6 +57,19 @@ class FilledTest extends TestCase {
         $validator->validate(['name' => null]);
 
         $this->assertEquals($validator->failed(), true);
+    }
+
+    public function testFailingFilledList()
+    {
+        $this->validator2->validate([
+            'people' => [
+                ['name' => 'John', 'age' => 42],
+                ['name' => '', 'age' => 38],
+                ['name' => 'Bar', 'age' => 25]
+            ]
+        ]);
+
+        $this->assertTrue($this->validator2->failed());
     }
 }
 ?>
