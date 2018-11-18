619e9a7ee9b7bb7c1057b008a0bff4fbb75deef2
khalyomede <>
2018-11-18 18:40:07 +0100

    added unit tests over list of lower strings

--- test/Validator/LowerTest.php
+++ test/Validator/LowerTest.php
@@ -3,6 +3,27 @@ use PHPUnit\Framework\TestCase;
 use Khalyomede\Validator;
 
 class LowerTest extends TestCase {
+    /**
+     * @var Khalyomede\Validator
+     */
+    protected $validator;
+
+    /**
+     * @var Khalyomede\Validator
+     */
+    protected $validator2;
+
+    public function setUp()
+    {
+        $this->validator = new Validator([
+            'words' => ['lower']
+        ]);
+
+        $this->validator2 = new Validator([
+            'sentences.*' => ['lower']
+        ]);
+    }
+
     public function testLower() {
         $validator = new Validator([
             'words' => ['lower']
@@ -15,6 +36,19 @@ class LowerTest extends TestCase {
         $this->assertEquals($validator->failed(), false);
     }
 
+    public function testLowerList()
+    {
+        $this->validator2->validate([
+            'sentences' => [
+                'do or do not there is no try',
+                'just for once let me look on you with my own eyes',
+                'i am just a simple man trying to make my way in the universe'
+            ]
+        ]);
+
+        $this->assertFalse($this->validator2->failed());
+    }
+
     public function testFailingLower() {
         $validator = new Validator([
             'words' => ['lower']
@@ -25,6 +59,19 @@ class LowerTest extends TestCase {
         ]);
 
         $this->assertEquals($validator->failed(), true);
+    }
+
+    public function testFailingLowerList()
+    {
+        $this->validator2->validate([
+            'sentences' => [
+                'Do. Or do not. There is no try.',
+                'Just for once, let me look on you with my own eyes.',
+                'I’m just a simple man trying to make my way in the universe.'
+            ]
+        ]);
+
+        $this->assertTrue($this->validator2->failed());
     }
 }
 ?>
