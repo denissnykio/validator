d507c797e72df31f9b6a999853fa4264822842a3
khalyomede <>
2018-11-18 18:45:16 +0100

    added unit tests over list of integers

--- test/Validator/IntegerTest.php
+++ test/Validator/IntegerTest.php
@@ -3,6 +3,27 @@ use PHPUnit\Framework\TestCase;
 use Khalyomede\Validator;
 
 class IntegerTest extends TestCase {
+    /**
+     * @var Khalyomede\Validator;
+     */
+    protected $validator;
+
+    /**
+     * @var Khalyomede\Validator;
+     */
+    protected $validator2;
+
+    public function setUp()
+    {
+        $this->validator = new Validator([
+            'age' => ['integer']
+        ]);
+
+        $this->validator2 = new Validator([
+            'people.*.age' => ['integer']
+        ]);
+    }
+
     public function testInteger() {
         $validator = new Validator([
             'age' => ['integer']
@@ -15,6 +36,19 @@ class IntegerTest extends TestCase {
         $this->assertEquals($validator->failed(), false);
     }
 
+    public function testIntegerList()
+    {
+        $this->validator2->validate([
+            'people' => [
+                ['firstname' => 'John', 'age' => 42],
+                ['firsname' => 'Foo', 'age' => 38],
+                ['firstname' => 'Bar', 'age' => 25]
+            ]
+        ]);
+
+        $this->assertFalse($this->validator->failed());
+    }
+
     public function testFailingInteger() {
         $validator = new Validator([
             'age' => ['integer']
@@ -25,6 +59,19 @@ class IntegerTest extends TestCase {
         ]);
 
         $this->assertEquals($validator->failed(), true);
+    }
+
+    public function testFailingIntegerList()
+    {
+        $this->validator2->validate([
+            'people' => [
+                ['firstname' => 'John', 'age' => 42],
+                ['firsname' => 'Foo'],
+                ['firstname' => 'Bar', 'age' => 25]
+            ]
+        ]);
+
+        $this->assertTrue($this->validator2->failed());
     }
 }
 ?>
