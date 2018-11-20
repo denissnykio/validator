ffdd27a6a72d7504e649461c4883dc9d452fb9d5
khalyomede <>
2018-11-20 23:16:04 +0100

    added unit tests over a list of string to validate against email rule

--- test/Validator/EmailTest.php
+++ test/Validator/EmailTest.php
@@ -3,28 +3,68 @@ use PHPUnit\Framework\TestCase;
 use Khalyomede\Validator;
 
 class EmailTest extends TestCase {
+    /**
+     * @var Khalyomede\Validation
+     */
+    protected $validator;
+
+    /**
+     * @var Khalyomede\Validation
+     */
+    protected $validator2;
+
+    public function setUp() {
+        $this->validator = new Validator([
+            'email' => ['email']
+        ]);
+
+        $this->validator2 = new Validator([
+            'people.*.email' => ['email']
+        ]);
+    }
+
     public function testEmail() {
-        $validator = new Validator(['email' => ['email']]);
+        $this->validator->validate(['email' => 'khalyomede@gmail.com']);
 
-        $validator->validate(['email' => 'khalyomede@gmail.com']);
+        $this->assertEquals($this->validator->failed(), false);
+    }
 
-        $this->assertEquals($validator->failed(), false);
+    public function testEmailList()
+    {
+        $this->validator2->validate([
+            'people' => [
+                ['name' => 'John', 'email' => 'john.doe@gmail.com'],
+                ['name' => 'Foo', 'email' => 'foo.bar@gmail.com'],
+                ['name' => 'Baz', 'email' => 'foo.baz@gmaiL.com']
+            ]
+        ]);
+
+        $this->assertFalse($this->validator2->failed());
     }
 
     public function testFailingEmail() {
-        $validator = new Validator(['email' => ['email']]);
+        $this->validator->validate(['email' => 42]);
 
-        $validator->validate(['email' => 42]);
-
-        $this->assertEquals($validator->failed(), true);
+        $this->assertEquals($this->validator->failed(), true);
     }
 
     public function testFailingEmail2() {
-        $validator = new Validator(['email' => ['email']]);
+        $this->validator->validate(['email' => 'khalyomede@gmail']);
 
-        $validator->validate(['email' => 'khalyomede@gmail']);
+        $this->assertEquals($this->validator->failed(), true);
+    }
 
-        $this->assertEquals($validator->failed(), true);
+    public function testFailingEmailList()
+    {
+        $this->validator2->validate([
+            'people' => [
+                ['name' => 'John', 'email' => 'john.doe@gmail.com'],
+                ['name' => 'Foo', 'email' => 'foo.bar@gmail'],
+                ['name' => 'Baz', 'email' => 'foo.baz@gmaiL.com']
+            ]
+        ]);
+
+        $this->assertTrue($this->validator2->failed());
     }
 }
 ?>
