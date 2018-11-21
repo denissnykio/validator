94bf95d90e4e64c42b9d9dc3daa453980ebbfe82
khalyomede <>
2018-11-21 22:22:00 +0100

    added unit tests over list of arrays

--- test/Validator/ArrayTest.php
+++ test/Validator/ArrayTest.php
@@ -3,20 +3,63 @@ use PHPUnit\Framework\TestCase;
 use Khalyomede\Validator;
     
 class ArrayTest extends TestCase {
+    /**
+     * @var Khalyomede\Validator
+     */
+    protected $validator;
+
+    /**
+     * @var Khalyomede\Validator
+     */
+    protected $validator2;
+
+    public function setUp()
+    {
+        $this->validator = new Validator([
+            'hobbies' => ['array']
+        ]);
+
+        $this->validator2 = new Validator([
+            'people.*.hobbies' => ['array']
+        ]);
+    }
+
     public function testArray() {
-        $validator = new Validator(['hobbies' => ['array']]);
+        $this->validator->validate(['hobbies' => ['programming', 'TV shows', 'workout']]);
 
-        $validator->validate(['hobbies' => ['programming', 'TV shows', 'workout']]);
+        $this->assertFalse($this->validator->failed());
+    }
 
-        $this->assertEquals($validator->failed(), false);
+    public function testArrayList()
+    {
+        $this->validator2->validate([
+            'people' => [
+                ['name' => 'John', 'hobbies' => ['programing', 'workout', 'watching tv shows']],
+                ['name' => 'Foo', 'hobbies' => ['dancing', 'drawing', 'learning self improvement']],
+                ['name' => 'Bar', 'hobbies' => ['photography', 'workout', 'politic']]
+            ]
+        ]);
+
+        $this->assertFalse($this->validator2->failed());
     }
 
     public function testFailingArray() {
-        $validator = new Validator(['hobbies' => ['array']]);
+        $this->validator->validate(['hobbies' => 'programming, TV shows, workout']);
 
-        $validator->validate(['hobbies' => 'programming, TV shows, workout']);
+        $this->assertTrue($this->validator->failed());
+    }
 
-        $this->assertEquals($validator->failed(), true);
+    public function testFailingArrayList()
+    {
+        $this->validator2->validate([
+            'people' => [
+                ['name' => 'John', 'hobbies' => ['programing', 'workout', 'watching tv shows']],
+                ['name' => 'Foo', 'hobbies' => 'dancing, drawing, learning self improvement'],
+                ['name' => 'Bar', 'hobbies' => ['photography', 'workout', 'politic']]
+            ]
+        ]);
+
+        $this->assertTrue($this->validator2->failed());
     }
 }
 ?>
